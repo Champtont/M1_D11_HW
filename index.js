fe244c6e2dd0f979917c67179aed85fd41a7dd76
Courtney Hampton <jaye0888@gmail.com>
2022-10-10 16:58:24 +0200

    hw
    As far as I got

--- index.js
+++ index.js
@@ -9,3 +9,27 @@ const pageBackground = function (color) {
 };
 
 pageBackground("lightblue");
+
+const fakeAddress = function (str) {
+  document.getElementById("address").innerHTML = str;
+};
+
+fakeAddress("123 fake rd. Nonexistant, Country 45678");
+
+const addCssClass = function () {
+  let ele = document.getElementsByTagName("a");
+  for (let i = 0; i < ele.length; i++) {
+    ele[i].classList.add("CSS");
+  }
+};
+
+addCssClass();
+
+const hideAllImg = function () {
+  let img = document.getElementsByClassName("img");
+  if (img.style.display === "block") {
+    img.style.display = "hidden";
+  } else {
+    img.style.display = "block";
+  }
+};
